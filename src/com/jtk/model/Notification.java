146307812b4a6d249b35eb6922519cfcb802a228
Deonetwo <48423286+deonetwo@users.noreply.github.com>
2020-05-05 12:57:56 +0700

    Update Model

--- src/com/jtk/model/Notification.java
+++ src/com/jtk/model/Notification.java
@@ -1,19 +1,33 @@
 package com.jtk.model;
 
+import java.time.LocalDateTime;
+
 public class Notification {
 	private User from;
 	private String message;
-	
+	private LocalDateTime time;
+
 	public User getFrom() {
 		return from;
 	}
+
 	public void setFrom(User from) {
 		this.from = from;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
-	} 
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
 }
