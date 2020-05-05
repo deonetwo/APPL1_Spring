ec2983083a640c1d4293a9ddf46aaccf36e741da
Deonetwo <48423286+deonetwo@users.noreply.github.com>
2020-05-05 15:58:39 +0700

    Update Configuration

--- src/com/jtk/config/MongoConfig.java
+++ src/com/jtk/config/MongoConfig.java
@@ -1,42 +1,23 @@
 package com.jtk.config;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-
+import org.springframework.data.mongodb.config.
+AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.
+EnableMongoRepositories;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 
 @Configuration
+@EnableMongoRepositories(basePackages = "orders.db")
 public class MongoConfig extends AbstractMongoConfiguration {
-
-	@Autowired
-	private Environment env;
-	
 	@Override
 	protected String getDatabaseName() {
 		return "userNotificationDB";
 	}
-	
-	public Mongo mongo() throws Exception {
-		MongoCredential credential =
-			MongoCredential.createMongoCRCredential(
-				env.getProperty("mongo.username"),
-				getDatabaseName(),
-				env.getProperty("mongo.password").toCharArray());
-		
-		return new MongoClient(
-			new ServerAddress("localhost", 27017), Arrays.asList(credential));
-	}
 
 	@Override
 	public MongoClient mongoClient() {
-		// TODO Auto-generated method stub
-		return null;
+		return new MongoClient("localhost" , 27017);
 	}
- }
+}
