f7021a090a2b7a3aa27adb679314ebc1b6e86712
Deonetwo <48423286+deonetwo@users.noreply.github.com>
2020-05-05 12:50:56 +0700

    Revert "Revert "model init""
    This reverts commit 0f59894f32cee17951458e2d4803a8cd0e39434e.

--- src/com/jtk/model/Notification.java
+++ src/com/jtk/model/Notification.java
@@ -1,5 +1,19 @@
 package com.jtk.model;
 
 public class Notification {
-
+	private User from;
+	private String message;
+	
+	public User getFrom() {
+		return from;
+	}
+	public void setFrom(User from) {
+		this.from = from;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	} 
 }
--- src/com/jtk/model/User.java
+++ src/com/jtk/model/User.java
@@ -1,5 +1,36 @@
 package com.jtk.model;
 
-public class User {
+import java.util.List;
 
+public class User {
+	private String id;
+	private String username;
+	
+	private List<User> friendList;
+	private List<Notification> notifList;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public List<User> getFriendList() {
+		return friendList;
+	}
+	public void setFriendList(List<User> friendList) {
+		this.friendList = friendList;
+	}
+	public List<Notification> getNotifList() {
+		return notifList;
+	}
+	public void setNotifList(List<Notification> notifList) {
+		this.notifList = notifList;
+	}
 }
