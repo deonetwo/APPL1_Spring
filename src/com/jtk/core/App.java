061f68935f3f01a3573aab8f95db02b0a1262396
Deonetwo <48423286+deonetwo@users.noreply.github.com>
2020-05-06 16:08:44 +0700

    Fixxed Tinggal Implement lagi

--- src/com/jtk/core/App.java
+++ src/com/jtk/core/App.java
@@ -7,7 +7,7 @@ public class App {
 	
     public static void main(String[] args) {
     	ApplicationContext context = new ClassPathXmlApplicationContext("resources/beans.xml");
-        AppMain app = (AppMain) context.getBean("appRun");
+    	AppInterface app = (AppInterface) context.getBean("app");
         
         app.userNotLogin();
     }
